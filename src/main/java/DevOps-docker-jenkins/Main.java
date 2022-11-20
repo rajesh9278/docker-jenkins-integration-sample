597252e6832848c15b8a9465c0ceb3dbab570c78
RAJESH VINAYAK PALANDE <109534902+rajesh9278@users.noreply.github.com>
2022-11-21 02:15:47 +0530

    Update Main.java

--- src/main/java/DevOps-docker-jenkins/Main.java
+++ src/main/java/DevOps-docker-jenkins/Main.java
@@ -2,7 +2,8 @@ package DevOps-docker-jenkins;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Mahalakshmi world!");
     }
 }
+# Test
 # Test
